f9630c9a12298e9bff13e8563f40611310921e69
Bùi Hữu Nghĩa <130369258+buihuunghia2004@users.noreply.github.com>
2024-09-20 08:14:12 +0700

    refactor: remove some file

--- src/services/apikey.service.js
+++
@@ -1,9 +0,0 @@
-const apiKeyModel = require("../models/apiKey.model")
-
-const findById = async (key) => {
-  return await apiKeyModel.findOne({ key, status: true }).lean()
-}
-
-module.exports = {
-  findById
-}
